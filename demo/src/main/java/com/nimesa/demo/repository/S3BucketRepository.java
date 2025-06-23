ecd095fe8f22ccd8013affdb90af45ed3cb371e0
ashutoshjain80 <53368425+ashutoshjain80@users.noreply.github.com>
2025-06-23 23:53:47 +0530

    Add files via upload

--- demo/src/main/java/com/nimesa/demo/repository/S3BucketRepository.java
+++ demo/src/main/java/com/nimesa/demo/repository/S3BucketRepository.java
@@ -3,16 +3,12 @@ package com.nimesa.demo.repository;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.nimesa.demo.entity.S3BucketDetails;
 
 public interface S3BucketRepository extends JpaRepository<S3BucketDetails, UUID>{
-     Page<S3BucketDetails> findAllByJobId(UUID jobId, Pageable pageable);
-     
        @Query("SELECT o.bucketName FROM S3BucketDetails o")
      List<String> findAllBuckets();
     
--- demo/src/main/java/com/nimesa/demo/repository/S3ObjectRepository.java
+++ demo/src/main/java/com/nimesa/demo/repository/S3ObjectRepository.java
@@ -1,5 +1,6 @@
 package com.nimesa.demo.repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -12,12 +13,11 @@ import com.nimesa.demo.entity.S3ObjectEntity;
 
 
 public interface S3ObjectRepository extends JpaRepository<S3ObjectEntity, UUID> {
-        long countByJobId(UUID jobId);
         long countByBucketName(String bucketName);
         List<S3ObjectEntity> findByBucketNameContaining(String searchPattern);
-        List<S3ObjectEntity> findByJobId(UUID jobId);
         List<S3ObjectEntity> findByBucketName(String bucketName);
         @Query("SELECT o.bucketName FROM S3ObjectEntity o where bucketName=:bucketName")
         List<String> findAllObjects(@Param("bucketName") String bucketName);
+        void deleteByObjectKeyIn( Collection<String> keys);
 }
 
