da0e4589bf2c9565acaa20e4b7e7e56ec310fc21
ashutoshjain80 <53368425+ashutoshjain80@users.noreply.github.com>
2025-06-23 16:06:35 +0530

    Add files via upload

--- demo/src/main/java/com/nimesa/demo/DemoApplication.java
+++ demo/src/main/java/com/nimesa/demo/DemoApplication.java
@@ -2,11 +2,17 @@ package com.nimesa.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.nimesa.demo"})
+@EntityScan(basePackages = {"com.nimesa.demo.entity"})
+@EnableJpaRepositories(basePackages = {"com.nimesa.demo.repository"})
 public class DemoApplication {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
 	}
