898a68118aadaa2713b28c3fdf9d7dc33f948d39
ashutoshjain80 <53368425+ashutoshjain80@users.noreply.github.com>
2025-06-23 21:45:58 +0530

    Add files via upload

--- demo/src/main/java/com/nimesa/demo/service/EC2BackupRestoreService.java
+++ demo/src/main/java/com/nimesa/demo/service/EC2BackupRestoreService.java
@@ -1,8 +1,13 @@
 package com.nimesa.demo.service;
 
 import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
 
 import com.amazonaws.services.s3.model.ListBucketsPaginatedRequest;
+import com.nimesa.demo.entity.EC2InstanceDetails;
+import com.nimesa.demo.entity.S3BucketDetails;
 import com.nimesa.demo.response.BackupResponse;
 
 import com.nimesa.demo.response.ImageStateResponse;
@@ -12,10 +17,13 @@ public interface EC2BackupRestoreService{
     List<BackupResponse> backupAllInstances();
     String restore(List<String> snapshotId,String imageId);
     List<ImageStateResponse> checkImageBackupStatus(List<String> imageId);
-    List<ImageStateResponse> discover();
-    JobStatusResponse discover_new();
+    JobStatusResponse discover_new(String servieName);
     JobStatusResponse listAllS3Buckets_new(ListBucketsPaginatedRequest paginatedRequest);
-    List<String> listAllS3Buckets(ListBucketsPaginatedRequest paginatedRequest);
     List<JobStatusResponse> getJobStatus(List<String> jobIds);
+    Page<EC2InstanceDetails> getEC2InstanceDetails(UUID jobId,int page,int size);
+    Page<S3BucketDetails> getS3BucketDetails(UUID jobId,int page ,int size);
+    JobStatusResponse createJobAndStoreS3Objects(String bucketName);
+
+
 
 }
