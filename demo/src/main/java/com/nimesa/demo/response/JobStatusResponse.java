bee66857737cc14a0f8a1f73e267e91f838fdc38
ashutoshjain80 <53368425+ashutoshjain80@users.noreply.github.com>
2025-06-23 17:25:41 +0530

    Add files via upload

--- demo/src/main/java/com/nimesa/demo/response/JobStatusResponse.java
+++ demo/src/main/java/com/nimesa/demo/response/JobStatusResponse.java
@@ -2,15 +2,26 @@ package com.nimesa.demo.response;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class JobStatusResponse {
     private UUID jobId;
     private String serviceName;
+    private String status;
+
+    public JobStatusResponse(UUID jobId, String status){
+        this.jobId=jobId;
+        this.status=status;
+    }
     
 }
