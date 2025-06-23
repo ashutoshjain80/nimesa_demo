dbb59414b4486041962ba33ff38ce1bea9eb1a9c
ashutoshjain80 <53368425+ashutoshjain80@users.noreply.github.com>
2025-06-23 14:52:52 +0530

    Add files via upload

--- demo/src/main/java/com/nimesa/demo/service/EC2BackupRestoreService.java
+++ demo/src/main/java/com/nimesa/demo/service/EC2BackupRestoreService.java
@@ -2,6 +2,7 @@ package com.nimesa.demo.service;
 
 import java.util.List;
 
+import com.amazonaws.services.s3.model.ListBucketsPaginatedRequest;
 import com.nimesa.demo.response.BackupResponse;
 
 import com.nimesa.demo.response.ImageStateResponse;
@@ -11,5 +12,6 @@ public interface EC2BackupRestoreService{
     String restore(List<String> snapshotId,String imageId);
     List<ImageStateResponse> checkImageBackupStatus(List<String> imageId);
     List<ImageStateResponse> discover();
+    List<String> listAllS3Buckets(ListBucketsPaginatedRequest paginatedRequest);
 
 }
